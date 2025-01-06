c77048a920c4e0194e900a5589cce1e94071eee3
Catherine Gilbert <cmg3@illinois.edu>
2025-01-06 12:10:08 -0600

    Update contest_messing.R

--- contest_messing.R
+++ contest_messing.R
@@ -3,9 +3,11 @@ library(FactoMineR)
 library(pheatmap)
 library(irlba)
 library(factoextra)
+library(viridis)
 
 setwd("C:/Users/cmg3/Documents/GitHub/UIUCDigitalHand")
 
+#GENERATE KINSHIP
 ng <- read_csv("C:/Users/cmg3/Downloads/numeric_genotype.csv")
 ng_dt <- ng %>% column_to_rownames("Hybrid")
 ng_mat <- ng %>% column_to_rownames("Hybrid") %>% as.matrix() %>% t()
@@ -22,6 +24,11 @@ ng_cov <- as.matrix(ng_cov_csv)
 ng_cor_csv <- read_csv("ng_cor.csv")
 ng_cor <- as.matrix(ng_cor_csv)
 
+ng_cor2 <- round(ng_cor, 2)
+image(ng_cor2, useRaster = TRUE, col = magma(100))
+
+
+#RUN PCA
 ng_pca <- irlba::prcomp_irlba(ng_cor, n = 20)
 
 summary(ng_pca)
@@ -33,9 +40,5 @@ fviz_screeplot(ng_pca, ncp = 20)
 fviz_pca_ind(ng_pca, label = "none")
 
 ng_pca_kmeans <- hkmeans(ng_pca$rotation, k = 10)
-fviz_cluster(ng_pca_kmeans)
+fviz_cluster(ng_pca_kmeans, geom = "point")
 
-library(viridis)
-
-ng_cor2 <- round(ng_cor, 2)
-image(ng_cor2, useRaster = TRUE, col = magma(100))
